4694e0244ca9c1b66e53d398b8ca1fa240991043
Dmitry Tolsky <dmitry.tolsky@rhinoda.com>
2018-10-03 15:13:20 +0600

    added models category, tag

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category, Tag
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'pub_date')
+    list_display = ('id', 'title', 'author', 'pub_date', 'category')
+
+
+admin.site.register(Category)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -4,11 +4,12 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=100)
-    body = models.TextField()
+    title = models.CharField(max_length=100, help_text='Post title')
+    body = models.TextField(help_text='Post content')
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     pub_date = models.DateField()
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    tag = models.ManyToManyField('Tag', help_text='Select a text for this post')
 
     def __str__(self):
         return self.title
@@ -19,16 +20,18 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post.detail', args=[str(self.id)])
 
-# 3. Category
-#     id = int
-#     name = varchar
-#
-# 4. Tag
-#     id = int
-#     name = varchar
-#
-# 5. Tag_Post
-#     tag_id = int
-#     post_id = int
-#
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 # 6. Comments
