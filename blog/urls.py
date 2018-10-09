443854ca343a5cb2d6e4b50f14744fdbb7976c22
Dmitry Tolsky <dima.tolsky@gmail.com>
2018-10-09 19:24:50 +0600

    added filter by category/author/tags and change profile

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,9 @@ from blog import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='posts.list'),
+    path('posts', views.post_filter, name='posts.filter'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post.detail'),
-    path('posts', views.post_filter),
+    path('post/create', views.post_create, name='post.create'),
+    path('post/<int:pk>/edit', views.post_edit, name='post.edit'),
+    path('post/<int:pk>/delete', views.post_delete, name='post.delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
@@ -5,18 +6,32 @@ from blog.models import Post
 
 
 def post_filter(request):
-    if 'author' in request.GET:
-        posts = Post.objects.filter(author=request.GET['author'])
-    elif 'category' in request.GET:
-        if request.GET['category'] == 'None':
-            posts = Post.objects.filter(category__isnull=True)
-        else:
-            posts = Post.objects.filter(category=request.GET['category'])
+    posts = Post.objects.all()
+    for filter in ['category', 'author', 'tag']:
+        if filter in request.GET:
+            if request.GET[filter] == 'None':
+                posts = posts.filter(**{filter + '__isnull': True})
+            else:
+                posts = posts.filter(**{filter: request.GET[filter]})
+
     return render(request, 'blog/post_list.html', {'post_list': posts})
 
 
 def profile(request):
-    return render(request, 'account/profile.html')
+    posts = Post.objects.filter(author=request.user.id)
+    return render(request, 'account/profile.html', {'posts': posts})
+
+
+def post_create(request):
+    return HttpResponse('Form for create new post')
+
+
+def post_edit(request, pk):
+    return HttpResponse('Form for edit post')
+
+
+def post_delete(request, pk):
+    return HttpResponse('Delete post')
 
 
 class PostListView(ListView):
